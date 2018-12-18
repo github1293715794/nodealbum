700057dfd31959f0f1ba69ed1d213e1c21dac580
github1293715794 <1293715794@qq.com>
2018-12-19 00:07:57 +0800

    app.js入口文件

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var router = require("./controller");// controller 中有package.json文件
+// var router = require("./controller/router.js");// controller中没有package.json文件
+
+// 设置模板引擎
+app.set("view engine", "ejs");
+
+//路由中间件
+// 静态页面
+// app.use("static", express.static("./public"));// 访问http://localhost:3000/static/js/jquery.js
+app.use(express.static("./public"));// 访问http://localhost:3000/js/jquery.js
+// get请求 首页
+app.get("/", router.showIndex);// get "/" 异步函数
+app.get("/:albumName", router.showAlbum);
+
+app.listen(3000);
