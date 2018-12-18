f98bd12eb54892872d23916448ffc12a49b2bead
github1293715794 <1293715794@qq.com>
2018-12-19 00:05:43 +0800

    controller控制器--路由文件

---
+++ controller/router.js
@@ -0,0 +1,8 @@
+var file = require("../models/file.js");
+exports.showIndex = function(req, res){
+  res.send("我是首页");
+}
+
+exports.showAlbum = function(req, res){
+  res.send("相册名字：" + req.params.albumName);
+}
